75f5cc2f5d1389b479581b093f58cde181c46d0c
djdt <10266332+djdt@users.noreply.github.com>
2017-08-29 18:07:45 +1000

    fix neovim requirment when vim used. Fixes #11

--- rplugin/python3/pyparens/__init__.py
+++ rplugin/python3/pyparens/__init__.py
@@ -1,18 +1,20 @@
-import neovim
+from importlib import find_loader
 
-from pyparens.pyparens import PyParens
+if not find_loader('vim'):
+    import neovim
+    from pyparens.pyparens import PyParens
 
 
-@neovim.plugin
-class PyParensHandler(object):
-    def __init__(self, vim):
-        self._vim = vim
-        self._pyparens = PyParens(self._vim)
+    @neovim.plugin
+    class PyParensHandler(object):
+        def __init__(self, vim):
+            self._vim = vim
+            self._pyparens = PyParens(self._vim)
 
-    @neovim.function('PyParensInit', sync=False)
-    def init(self, args):
-        self._pyparens.init()
+        @neovim.function('PyParensInit', sync=False)
+        def init(self, args):
+            self._pyparens.init()
 
-    @neovim.function('PyParensMatch', sync=False)
-    def match(self, args):
-        self._pyparens.match()
+        @neovim.function('PyParensMatch', sync=False)
+        def match(self, args):
+            self._pyparens.match()
